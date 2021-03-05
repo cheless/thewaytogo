e2e3d59226a1c07348ef3ede1d215e598ad914a3
Unknown <chyc09@163.com>
2021-03-05 18:25:15 +0800

    Initial commit

---
+++ examples/chapter_6/anonymous_func.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	for i := 0; i < 10; i++ {
+		t := func(){fmt.Printf("The number is: %d\n", i)}
+		t()
+		fmt.Printf("t is of type %T and has value %v\n", t, t)
+	}
+}
